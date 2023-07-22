a42be659ad1a19a88a9b5e03f98c8a5be5b72088
astemirov <kirillastemirov@mail.ru>
2023-07-22 20:50:51 +0300

    7kyu

--- 7 kyu/anagram_detection.py
+++ 7 kyu/anagram_detection.py
@@ -1,10 +1,6 @@
-
 # Solution 1
 def is_anagram(test, original):
     return set(test.lower()) == set(original.lower()) and len(test) == len(original)
-
-
-
 
 # Solution 2
 def is_anagram(test, original):
