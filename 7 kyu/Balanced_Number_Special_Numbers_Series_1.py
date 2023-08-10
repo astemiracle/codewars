e80b769c0d8ca83be448b914e53499750f774976
astemirov <kirillastemirov@mail.ru>
2023-08-10 09:29:49 +0300

    7kyu

--- 7 kyu/Balanced_Number_Special_Numbers_Series_1.py
+++ 7 kyu/Balanced_Number_Special_Numbers_Series_1.py
@@ -14,3 +14,18 @@ def balanced_num(number):
         else:
             return "Not Balanced"
 
+
+
+# Solution 2
+def balanced_num(number):
+    return '%sBalanced' % ( 'Not ' if (sum(int(e) for e in str(number)[:(len(str(number))+1)//2-1]) !=  sum(int(e) for e in str(number)[(len(str(number)))//2+1:])) else '' )
+
+
+
+
+# Solution 3
+def balanced_num(number):
+    s = str(n)
+    l = (len(s)-1)//2
+    same = len(s) < 3 or sum(map(int, s[:l])) == sum(map(int, s[-l:]))
+    return "Balanced" if same else "Not Balanced"
